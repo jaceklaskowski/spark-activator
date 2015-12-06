15404769606eb553f6b6819b6742a132bf62c2a8
Jacek Laskowski <jacek@japila.pl>
2015-12-06 09:30:15 +0100

    Latest versions of sbt and library dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,14 @@
 import sbt._
 
 object Version {
-  val akka      = "2.3.9"
-  val hadoop    = "2.6.0"
-  val logback   = "1.1.2"
+  val akka      = "2.4.1"
+  val hadoop    = "2.7.1"
+  val logback   = "1.1.3"
   val mockito   = "1.10.19"
-  val scala     = "2.11.5"
-  val scalaTest = "2.2.4"
+  val scala     = "2.11.7"
+  val scalaTest = "2.2.5"
   val slf4j     = "1.7.6"
-  val spark     = "1.2.0"
+  val spark     = "1.5.2"
 }
 
 object Library {
